42ceafe77fd1575f6449f1201cce110e5871f8a0
hp <niranjan962796@gmail.com>
2024-10-07 18:41:05 +0530

    first Commit

---
+++ src/Card.js
@@ -0,0 +1,13 @@
+
+// src/Card.js
+import React from 'react';
+
+const Card = ({ title }) => {
+  return (
+    <div className="card">
+      <h3>{title}</h3>
+    </div>
+  );
+};
+
+export default Card;
